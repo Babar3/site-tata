0c61041ce3f1091138fd24ea31051e7e300ce355
Babar3 <axel.lepine@master-developpement-logiciel.fr>
2021-10-10 20:12:07 +0200

    Image slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'site-tata';
+  imageObject = [{
+    image: './assets/images/gosses1.jpg',
+    thumbImage: './assets/images/gosses1.jpg',
+    title: 'conter'
+}, {
+    image: './assets/images/gosses2.jpg',
+    thumbImage: './assets/images/gosses2.jpg',
+    title: 'flatearther.jpg'
+}, {
+    image: './assets/images/gosses3.jpg',
+    thumbImage: './assets/images/gosses3.jpg',
+    title: 'pinture'
+},{
+    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/7.jpg',
+    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/7.jpg',
+    title: 'Hummingbirds are amazing creatures'
+}, {
+    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/1.jpg',
+    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/1.jpg'
+}, {
+    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/2.jpg',
+    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/2.jpg',
+    title: 'Example two with title.'
+}];
 }
